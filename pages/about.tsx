fa819ea6b1033f9056975e66ba0d9bdabe47a118
Atish Maharjan <atish@opencafe.io>
2022-12-13 10:53:40 +0545

    initial commit

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/system';
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <Box>
+        <p>This is a app to find DJ Events</p>
+      </Box>
+    </div>
+  );
+}
